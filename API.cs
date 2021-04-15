7353e0407de01bd9eedb9625107e13ea189c9d93
Lucas7yoshi <Lucas7yoshi@gmail.com>
2021-04-15 19:58:40 -0400

    Add project files.

---
+++ API.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace hShopCLI
+{
+    class hShop
+    {
+        //urls
+        private const string APIbase = "https://hshop.erista.me/api/";
+        private const string index = "https://hshop.erista.me/api/index";
+        private const string APIbase = "https://hshop.erista.me/api/index";
+
+
+        public hShop()
+        {
+            //Initialize the API
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace hShopCLI
+{
+    class Program
+    {
+        private string version = "0.0.1";
+        static void Main(string[] args)
+        {
+            Console.WriteLine("hShop CLI v{version} by Lucas7yoshi");
+            Console.WriteLine("This ")
+            Console.WriteLine("Please select version:");
+        }
+    }
+}
